fb714ff7db68ebec90c70ccd4303f6573d37e0a9
Akant <akantmalviya786@gmail.com>
2023-07-06 02:19:47 +0530

    final commit

--- got.js
+++ got.js
@@ -1,7 +1,6 @@
 function fetchData(){
-
     xhr= new XMLHttpRequest();
-    xhr.open("GET","https://www.goodreads.com/work/quotes/1466917-a-game-of-thrones");
+    xhr.open("GET","https://api.gameofthronesquotes.xyz/v1/random/5");
     xhr.send();
     var data;
     xhr.onreadystatechange = function() {
@@ -11,12 +10,16 @@ function fetchData(){
          }
         if (this.readyState == 4 && this.status == 200) {
         data=JSON.parse(this.responseText);
-         console.log(data);
+        console.log(data);
             
-    var a=document.createElement("div");   
-    var name=document.createElement("li");
-    name.innerHTML=data.quote;
-    a.appendChild(name);
+    let a=document.createElement("div");   
+    for(let i=0; i< data.length; i++)
+    {   let name =  document.createElement("p");
+        name.innerHTML=  data[i].sentence;
+        console.log( data[i].sentence);
+        a.appendChild(document.createElement("br"));
+        a.appendChild(name);
+    }
+        
     document.getElementById("Results").appendChild(a);   }  };
 }
-            
